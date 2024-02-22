bcfb1f45b4016114747888c0ec5df72673853278
huhao <huhao@mediinfo.com.cn>
2024-02-22 23:12:16 +0800

    feat: hono 替代 oak

--- main.ts
+++ main.ts
@@ -1,33 +1,32 @@
-/// <reference lib="deno.unstable" />
+import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts"
+import data from "./data.json" assert { type: "json" }
 
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
-import data from "./data.json" assert { type: "json" };
+const app = new Hono()
+// const kv = await Deno.openKv();
 
-const router = new Router();
-router
-  .get("/", (context) => {
-    context.response.body = "Welcome to dinosaur API!";
-  })
-  .get("/api", (context) => {
-    context.response.body = data;
-  })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const found = data.find((item) =>
-        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (found) {
-        context.response.body = found;
-      } else {
-        context.response.body = "No dinosaurs found.";
-      }
+app.get("/", (c) => {
+  return c.json("Welcome to dinosaur API!")
+})
+
+app.get("/api", (c) => {
+  return c.json(data)
+})
+
+app.get("/api/:dinosaur", (c) => {
+  const dinosaur = c.req.param("dinosaur")
+
+  if (dinosaur) {
+    const found = data.find(
+      (item) => item.name.toLowerCase() === dinosaur.toLowerCase()
+    )
+    if (found) {
+      return c.json(found)
+    } else {
+      return c.json("No dinosaurs found.")
     }
-  });
+  } else {
+    return c.json("No dinosaurs found.")
+  }
+})
 
-const app = new Application();
-app.use(oakCors()); // Enable CORS for All Routes
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-await app.listen({ port: 8000 });
+Deno.serve(app.fetch)
