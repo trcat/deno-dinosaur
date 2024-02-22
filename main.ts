43c93fd7441bc93d13c88f0e54a4f98fd1b4b69d
huhao <huhao@mediinfo.com.cn>
2024-02-22 23:44:08 +0800

    feat: 添加待办事项相关 restful api

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts"
+
 import data from "./data.json" assert { type: "json" }
 
 const app = new Hono()
-// const kv = await Deno.openKv();
+const kv = await Deno.openKv()
 
 app.get("/", (c) => {
   return c.json("Welcome to dinosaur API!")
@@ -27,6 +28,45 @@ app.get("/api/:dinosaur", (c) => {
   } else {
     return c.json("No dinosaurs found.")
   }
+})
+
+/**
+ * 获取所有待办事项
+ */
+app.get("/todo", async (c) => {
+  const iter = await kv.list({ prefix: ["todo"] })
+  const todo = []
+  for await (const res of iter) todo.push(res)
+
+  return c.json(todo)
+})
+
+/**
+ * 创建待办事项
+ */
+app.post("/todo", async (c) => {
+  const body = await c.req.json()
+  const id = `todo_${Date.now()}`
+  const result = await kv.set(["todo", id], Object.assign(body, { id }))
+  return c.json(result)
+})
+
+/**
+ * 修改待办事项
+ */
+app.put("/todo/:id", async (c) => {
+  const id = c.req.param("id")
+  const result = await kv.set(["todo", id], await c.req.json())
+  return c.json(result)
+})
+
+/**
+ * 删除待办事项
+ */
+app.delete("/todo/:id", async (c) => {
+  const id = c.req.param("id")
+  await kv.delete(["todo", id])
+  return c.text("删除成功")
 })
 
 Deno.serve(app.fetch)
